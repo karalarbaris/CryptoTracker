12d1ea7e33121d1c47408c269522ebd095039285
Baris Karalar <karalarbaris@gmail.com>
2023-09-10 18:15:21 +0200

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Baris Karalar on 10.09.23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
