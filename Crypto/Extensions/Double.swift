d06ef56403b9025bfb33f82720cd09edbede29df
Baris Karalar <karalarbaris@gmail.com>
2023-09-13 15:55:35 +0200

    Design a row to display coins in a List

---
+++ Crypto/Extensions/Double.swift
@@ -0,0 +1,59 @@
+//
+//  Double.swift
+//  Crypto
+//
+//  Created by Baris Karalar on 13.09.23.
+//
+
+import Foundation
+
+extension Double {
+
+    /// Converts a Double into a Currency with 2 decimal places
+    private var currencyFormatter2: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+//        formatter.locale = .current // current value
+//        formatter.currencyCode = "usd"
+//        formatter.currencySymbol = "$"
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }
+    
+    /// Converts a Double into a Currency as a String with 2-4 decimal places
+    func asCurrencyWith2Decimals() -> String {
+        let number = NSNumber(value: self)
+        return currencyFormatter2.string(from: number) ?? "$0.00"
+    }
+    
+    /// Converts a Double into a Currency with 2-6 decimal places
+    private var currencyFormatter6: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+//        formatter.locale = .current // current value
+//        formatter.currencyCode = "usd"
+//        formatter.currencySymbol = "$"
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 6
+        return formatter
+    }
+    
+    /// Converts a Double into a Currency as a String with 2-6 decimal places
+    func asCurrencyWith6Decimals() -> String {
+        let number = NSNumber(value: self)
+        return currencyFormatter6.string(from: number) ?? "$0.00"
+    }
+    
+    /// Converts Double as String representation
+    func asNumberString() -> String {
+        return String(format: "%.2f", self)
+    }
+    
+    /// Converts Double as String representation with percent symbol
+    func asPercentString() -> String {
+        return asNumberString() + "%"
+    }
+}
