6e4e9b9558ba349216e1c0140d1e6685908fef72
Baris Karalar <karalarbaris@gmail.com>
2023-09-25 21:52:46 +0300

    Add a dynamic List to display downloaded coins

---
+++ Crypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewViewModel.swift
+//  Crypto
+//
+//  Created by Baris Karalar on 25.09.23.
+//
+
+import Foundation
+
+class HomeViewViewModel: ObservableObject {
+    
+    
+    
+}
