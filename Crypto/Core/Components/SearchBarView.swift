45de0a5c83d5118c4b09a2c4f1a34819c3ce6742
Baris Karalar <karalarbaris@gmail.com>
2023-10-19 10:34:27 +0300

    Implement a custom and reusable search bar

--- Crypto/Core/Components/SearchBarView.swift
+++ Crypto/Core/Components/SearchBarView.swift
@@ -8,11 +8,49 @@
 import SwiftUI
 
 struct SearchBarView: View {
+    
+    @Binding var searchText: String
+    @FocusState private var searchTextIsFocused: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Image(systemName: "magnifyingglass")
+                .foregroundStyle(
+                    searchText.isEmpty ? Color.theme.secondaryText : Color.theme.accent
+                )
+            
+            TextField("Search by name of symbol", text: $searchText)
+                .foregroundStyle(Color.theme.accent)
+                .autocorrectionDisabled(true)
+                .focused($searchTextIsFocused)
+                .overlay(alignment: .trailing, content: {
+                    Image(systemName: "xmark.circle.fill")
+                        .padding()
+                        .offset(x: 10.0)
+                        .foregroundStyle(Color.theme.accent)
+                    //                        .background(Color.red)
+                        .opacity(
+                            searchText.isEmpty ? 0.0 : 1.0
+                        )
+                        .onTapGesture {
+                            searchText = ""
+                            searchTextIsFocused = false
+                        }
+                })
+            
+        }
+        .font(.headline)
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 25)
+                .fill(Color.theme.background)
+                .shadow(color: Color.theme.accent.opacity(0.15), radius: 10)
+        )
+        .padding()
     }
 }
-
-#Preview {
-    SearchBarView()
+#Preview(traits: .sizeThatFitsLayout) {
+    SearchBarView(searchText: .constant(""))
+        .previewLayout(.sizeThatFits)
+        .preferredColorScheme(.dark)
 }
